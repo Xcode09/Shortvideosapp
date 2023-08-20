04b0205afaa82789688b14b83ab4855338e4beb3
Muhammad Ali Rana <alisaleem877@gmail.com>
2023-08-20 22:56:55 +0500

    Initial Commit

---
+++ Short Videos App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Short Videos App
+//
+//  Created by Berisco Pvt on 20/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
