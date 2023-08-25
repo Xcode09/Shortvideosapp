7cac0654e29713dcb093c42c6ec3e5439d0ec12d
Muhammad Ali Rana <alisaleem877@gmail.com>
2023-08-25 22:46:34 +0500

    First Changes

--- Short Videos App/NunitoFonts/NunitoFonts.swift
+++ Short Videos App/NunitoFonts/NunitoFonts.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+import UIKit
+struct NunitoFonts{
+    static let nunitoBold = UIFont(name: "Nunito-Bold", size: 20)
+    static let nunitoSemiBold = UIFont(name: "Nunito-SemiBold", size: 18)
+    static let nunitoRegularBold = UIFont(name: "Nunito-Regular", size: 17)
+    static let nunitoLight = UIFont(name: "Nunito-Light", size: 17)
+}
